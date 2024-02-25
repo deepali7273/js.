544fe51394964aa562b767bb1fa5461df4003845
deepali7273 <152967891+deepali7273@users.noreply.github.com>
2024-02-25 07:11:51 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score=44
+
+
+
+console.log(typeof score);
+
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=33
+//"33acd"=NaN
+//"true"=1
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
