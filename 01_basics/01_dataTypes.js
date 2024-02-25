ee0d6becd49417355a46dd3a8f0b4f304d8ff565
deepali7273 <152967891+deepali7273@users.noreply.github.com>
2024-02-25 06:50:44 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(5+9)
+
+let name="deepali"
+let age=20
+let isLoggedIn=true
+let state=null
+
+// null = standalone value
+// undefined
+// symbol = unique
+
+// object
+
+console.log(typeof null);
